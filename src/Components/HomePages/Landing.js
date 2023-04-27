6f6261a70a48517124f695e53f0aa115f366fe95
Christina Bui <uyen.buitie@gmail.com>
2023-04-27 16:01:07 -0500

    NewLanding

--- src/Components/HomePages/Landing.js
+++ src/Components/HomePages/Landing.js
@@ -1,38 +1,31 @@
 import React, { Component } from 'react';
 import Fade from 'react-reveal/Fade';
 import {Route, Link, BrowserRouter} from 'react-router-dom';
-
+import logo from '../../Assets/horizontalLogo.svg';
 class Landing extends Component {
     state = {  } 
     render() { 
         return (
-            <div id = "home-landing" className='center-container cb-fullpage'>
+            <div id = "home-landing" className='cb-fullpage'>
+                <div className='col-lg-6'>
+                    <img src={logo}/>
+                    <p className='leading cb-paragraph'>The best All Nude Strip Club with all exclusive experience with gorgeous women.</p>
+                    <p className='sub-leading location cb-paragraph'><u>We have two lovely locations:</u></p>
+                    <p className='sub-leading cb-paragraph'>10 miles from Joplin, Missouri & Directly across from Downstream Casino (Galena, Kansas)</p>
+                    <p className='sub-leading cb-paragraph'>15 miles East of Hard Rock Cafe in Tulsa, Oklahoma (Inola, Oklahoma)</p>
+                    <div className='button-book'>
+                        <Link class = "navItems"
+                                onClick={() => {
+                                                document.querySelector("html").scrollTo(0,0);
+                                                }}
+                                                to = "/Book">
+                            <div className='navItem cb-paragraph'><button style={{fontSize: "35px"}}>Book Now</button></div>
+                            </Link>
+                            <p className='sub-leading cb-paragraph'>Book Online for exclusive discounts, bundles and more.</p>
 
-                <div className='text-center'>
-                   <Fade><p className='fancy-heading'>Sensations Gentleman's Club</p></Fade> 
-                   <div className='location-names'>
-                        <p className='fancy-paragraph'><b>Inola, Oklahoma</b></p>
-                        <p className='fancy-paragraph'>|</p>
-                        <p className='fancy-paragraph'><b>Galena, Kansas</b></p>
                     </div>
-
-                    <p className='fancy-subheading'>Book now to see our sexy ladies in our gentlemen's club.</p>
-                    
-                    
-                   
-                   
-                    <Link class = "navItems"
-                            onClick={() => {
-                                            document.querySelector("html").scrollTo(0,0);
-                                            }}
-                                            to = "/Book">
-                        <div className='navItem cb-paragraph'><button style={{fontSize: "30px"}}>Book Now</button></div>
-                        </Link>
-                        <p className='fancy-paragraph'>See our exclusive package deals and bundles we offer for online booking</p>
-                        
-                   
-                   
                 </div>
+         
             </div>
         );
     }
